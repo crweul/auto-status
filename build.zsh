40fbae4441db90bb1af58dc341ccf1762f8850bf
Kylo <109703063+laewliet@users.noreply.github.com>
2026-02-11 17:55:41 +0100

    Update

--- build.zsh
+++ build.zsh
@@ -5,6 +5,6 @@ script_dir="${0:A:h}"
 
 cd "$script_dir"
 
-dotnet build AutoStatus/AutoStatus.csproj -c Release
+dotnet build auto-status/AutoStatus.csproj -c Release
 
-echo "Build complete. Output in AutoStatus/bin/Release/"
+echo "Build complete. Output in auto-status/bin/Release/"
